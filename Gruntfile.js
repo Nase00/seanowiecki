ed6ccf220c2bcce816c9f5b5f7b2872bdec38359
nase00 <nase00@gmail.com>
2015-04-04 13:36:17 -0500

    Install Grunt and dependencies.

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+module.exports = function(grunt) {
+  var cleanCssOptions;
+
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+ 
+  grunt.initConfig({
+
+    connect: {
+      all: {
+        options:{
+          port: 9000, // keepalive server is 8000
+          hostname: "localhost",
+          keepalive: true
+        },
+      },
+      keepalive: true,
+    },
+    less: {
+      development: {
+        options: {
+          paths: ["./assets/"],
+          yuicompress: true
+        },
+        files: {
+          "./assets/css/application.css": "./assets/less/application.less"
+        },
+      },
+    },
+    karma: {
+      unit: {
+        options: {
+          files: ['test/**/*.js']
+        },
+      },
+    },
+    watch: {
+      files: "./assets/less/*.less",
+      tasks: ["less"],
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-karma');
+
+  grunt.event.on('watch', function(action, filepath, target) {
+    grunt.log.writeln(target + ': ' + filepath + ' has ' + action);
+  });
+
+  grunt.registerTask('server',[
+    'connect:keepalive',
+    'watch',
+  ]);
+};
